bb0a5709e3ab30527f08eedfd16e314e9e7ffb60
Kelvin Li <jpkelvinli@gmail.com>
2019-01-04 17:35:03 -0800

    Move map data to asset folder.

--- src/map.js
+++ src/map.js
@@ -1,8 +1,7 @@
 import Plot from './plot';
-import result from "./usa.topo.json";
-
 import {geoPath} from 'd3-geo';
 import * as topojson from 'topojson-client';
+import usaTopo from "../assets/usa.topo.json";
 
 export default class Map extends Plot {
     constructor(data, view) {
@@ -15,14 +14,13 @@ export default class Map extends Plot {
         this.svg.main.append("g")
             .attr("class", "states")
             .selectAll("path")
-            .data(topojson.feature(result, result.objects.states).features)
+            .data(topojson.feature(usaTopo, usaTopo.objects.states).features)
             .enter().append("path")
                 .attr("d", path);
     
         this.svg.main.append("path")
             .attr("class", "state-borders")
-            .attr("d", path(topojson.mesh(result, result.objects.states, function(a, b) { return a !== b; })));
-
+            .attr("d", path(topojson.mesh(usaTopo, usaTopo.objects.states, function(a, b) { return a !== b; })));
 
     }
 }
